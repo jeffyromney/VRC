c5059016373ac475a1c036b1e6c3b3de5997c8c2
Jefferson Romney <jeffyromney@gmail.com>
2015-10-18 23:10:46 -0400

    Started Adding security, Added custom labels on form for volunteers

--- vrc/settings.py
+++ vrc/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
--- vrc/urls.py
+++ vrc/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from vrc import views
 from django.contrib import admin
 admin.autodiscover()
+from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,6 +10,11 @@ urlpatterns = patterns('',
     url(r'^addVolunteer/$', 'vrc.views.addVolunteer', name='addVolunteer'),
     url(r'^addOrganization/$', 'vrc.views.addOrganization', name='addOrganization'),
     url(r'^addJob/$', 'vrc.views.addJob', name='addJob'),
+    url(r'^search/$', 'vrc.views.search', name='Search'),
+    url(r'^Volunteer/view/(\d{1,2})/$', 'vrc.views.view', name='View Volunteer'),
+    url(r'^accounts/login/$',  login),
+    url(r'^accounts/logout/$', logout),
+    url(r'^accounts/profile/$', 'vrc.views.loginSuccess'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
